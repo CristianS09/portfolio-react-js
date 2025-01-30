cff5b59975c32c51b66e234df8e10928fdeb5f64
CristianS09 <redentorking90@gmail.com>
2025-01-30 13:35:56 -0300

    adicionando novo projeto

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -2,7 +2,8 @@ const projectsData = [
     { id: 1, title: 'Cadastro de Fornecedor👷', subtitle: 'Aplicativo de cadastro de fornecedor em React Native⚛️.', images: ['/images/fornecedor1.jpg', '/images/fornecedor2.jpg'], link: 'https://github.com/CristianS09/cadastro-fornecedor-react-native.git', alt: 'App Fornecedor React Native' },
     { id: 2, title: 'Agência de Viagem✈️', subtitle: 'Aplicativo de Agência de viagem em Flutter📱.', images: ['/images/app-destino1.jpeg', '/images/app-destiono2.jpeg'], link: 'https://github.com/CristianS09/agencia-viagem-flutter.git', alt: 'App Agencia de Viagem' },
     { id: 3, title: 'Agenda📝', subtitle: 'Aplicação web de agenda em Java☕.', images: ['/images/agenda1.jpeg', '/images/agenda2.jpeg'], link: 'https://github.com/CristianS09/agenda-java-web.git', alt: 'App Agenda' },
-    { id: 4, title: 'My Weather🌧️', subtitle: 'Aplicação de previsão do tempo em React Native⚛️.', images: ['/images/my-weather1.jpg', '/images/my-weather2.jpg'], link: 'https://github.com/CristianS09/my-weather.git', alt: 'App Previsão do Tempo' }
+    { id: 4, title: 'My Weather🌧️', subtitle: 'Aplicação de previsão do tempo em React Native⚛️.', images: ['/images/my-weather1.jpg', '/images/my-weather2.jpg'], link: 'https://github.com/CristianS09/my-weather.git', alt: 'App Previsão do Tempo' },
+    { id: 5, title: 'Jojo Bizarre Adventure Api⭐', subtitle: 'Api de Jojos Bizarre Adventure desenvolvida em Java e Spring Boot🍃 .', images: ['/images/api1.png', '/images/api2.png'], link: 'https://github.com/CristianS09/jojo-api-spring-boot.git', alt: 'Api de Jojos Bizarre Adventure' },
 ];
 
 export default projectsData;
