d228c19a58ada5f47f51ca43eb568e3146720b3f
CristianS09 <redentorking90@gmail.com>
2025-02-07 17:13:15 -0300

    Adicionado o projeto de conversor de moeda

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -3,7 +3,8 @@ const projectsData = [
     { id: 2, title: 'Agência de Viagem✈️', subtitle: 'Aplicativo de Agência de viagem em Flutter📱.', images: ['/images/app-destino1.jpeg', '/images/app-destiono2.jpeg'], link: 'https://github.com/CristianS09/agencia-viagem-flutter.git', alt: 'App Agencia de Viagem' },
     { id: 3, title: 'Agenda📝', subtitle: 'Aplicação web de agenda em Java☕.', images: ['/images/agenda1.jpeg', '/images/agenda2.jpeg'], link: 'https://github.com/CristianS09/agenda-java-web.git', alt: 'App Agenda' },
     { id: 4, title: 'My Weather🌧️', subtitle: 'Aplicação de previsão do tempo em React Native⚛️.', images: ['/images/my-weather1.jpg', '/images/my-weather2.jpg'], link: 'https://github.com/CristianS09/my-weather.git', alt: 'App Previsão do Tempo' },
-    { id: 5, title: 'Jojo Bizarre Adventure Api⭐', subtitle: 'Api de Jojos Bizarre Adventure desenvolvida em Java e Spring Boot🍃 .', images: ['/images/api1.png', '/images/api2.png'], link: 'https://github.com/CristianS09/jojo-api-spring-boot.git', alt: 'Api de Jojos Bizarre Adventure' },
+    { id: 5, title: 'Jojo´s Bizarre Adventure Api⭐', subtitle: 'Api de personagens de Jojo´s Bizarre Adventure desenvolvida em Java e Spring Boot🍃 .', images: ['/images/api1.png', '/images/api2.png'], link: 'https://github.com/CristianS09/jojo-api-spring-boot.git', alt: 'Api de Jojo´s Bizarre Adventure' },
+    { id: 6, title: 'Conversor de Moeda💲', subtitle: 'App de conversão de moeda desenvolvido com React JS e Typescript💻', images: ['/images/conversor-1.png', '/images/conversor-2.png'], link: 'https://github.com/CristianS09/conversor-react-js.git', alt: 'App conversão de moeda' },
 ];
 
 export default projectsData;
